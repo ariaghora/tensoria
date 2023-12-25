fadc0779f269584b7ce8622c94971eaeeeeb91d8
Aria Ghora Prabono <hello@ghora.net>
2023-12-26 00:12:02 +0900

    feat(wgsl): Implement mul_backward.wgsl
    Adds the mul_backward.wgsl file to the src/wgpu/wgsl directory. The file contains the implementation of the main function, which calculates the backward propagation for the multiplication operation.
    
    The function takes in several input and gradient arrays and computes the gradients for the left and right operands based on the output gradient. The computation is done in parallel using the global_invocation_id.
    
    The left and right gradients are calculated by multiplying the output gradient with the corresponding input operand.
    
    This file is needed for the backward pass of the matmul operation.
    
    feat(op): Add setup_shader_backward for matmul.rs
    
    Adds the setup_shader_backward method to the OpMatmul struct in the src/wgpu/op/matmul.rs file. This method is responsible for setting up the shader code for the backward pass of the matmul operation.
    
    Currently, the method is not implemented and returns a "todo!" placeholder. This method will be implemented in a future commit to

--- src/wgpu/op/matmul.rs
+++ src/wgpu/op/matmul.rs
@@ -7,7 +7,7 @@ use crate::wgpu::op::Op;
 pub struct OpMatmul {}
 
 impl Op for OpMatmul {
-    fn setup_shader(&self, id: Uuid, session: &Session, params: &mut Context) {
+    fn setup_shader_forward(&self, id: Uuid, session: &Session, params: &mut Context) {
         let op = &session.variables.borrow()[&id];
         let left = &session.variables.borrow()[&op.prevs[0]];
         let right = &session.variables.borrow()[&op.prevs[1]];
@@ -17,7 +17,11 @@ impl Op for OpMatmul {
         params.insert("M", &left.shape[0]);
         params.insert("N", &right.shape[1]);
         params.insert("K", &left.shape[1]);
-        params.insert("n_unroll", &8);
+        params.insert("n_unroll", &1);
+    }
+
+    fn setup_shader_backward(&self, id: Uuid, session: &Session, params: &mut Context) {
+        todo!()
     }
 
     fn workgroups(&self, id: Uuid, session: &Session) -> [u32; 3] {
@@ -28,6 +32,8 @@ impl Op for OpMatmul {
         let n = out_shape[1];
         let num_workgroups_x = (n + local_size_x_y - 1) / local_size_x_y;
         let num_workgroups_y = (m + local_size_x_y - 1) / local_size_x_y;
-        [num_workgroups_x as u32, num_workgroups_y as u32, 1]
+        let wg = [num_workgroups_x as u32, num_workgroups_y as u32, 1];
+        let wg = [(m as f32 / local_size_x_y as f32).ceil() as u32, (n as f32 / local_size_x_y as f32).ceil() as u32, 1];
+        return wg;
     }
 }
