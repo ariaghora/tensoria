8441118e6219a7ca488a2fa46a318964da2ce02d
Aria Ghora Prabono <hello@ghora.net>
2024-01-15 16:10:06 +0900

    fix(tensor): wrong grad_fn for mean across all axes

--- src/nn/mod.rs
+++ src/nn/mod.rs
@@ -1,4 +1,5 @@
 use std::fmt::Debug;
+use std::marker::PhantomData;
 use std::ops::Add;
 use std::sync::{Arc, RwLock};
 
@@ -34,8 +35,7 @@ impl<T> Linear<T>
     pub fn new(in_size: usize, out_size: usize) -> Result<Self, TensoriaError> {
         let mut rng = rand::thread_rng();
 
-        let xavier_limit =
-            (6.0f32 / ((in_size + out_size) as f32).sqrt());
+        let xavier_limit = (6.0f32 / ((in_size + out_size) as f32).sqrt());
         let uniform = Uniform::new(-xavier_limit, xavier_limit);
 
         let w_val = (0..in_size * out_size)
@@ -81,10 +81,71 @@ impl<T> Module<T> for Linear<T>
     }
 }
 
+pub struct Sequential<M, T> {
+    modules: Vec<M>,
+    _p: PhantomData<T>,
+}
+
+impl<M, T> Sequential<M, T>
+    where
+        T: TensoriaOps + Clone + Pod + Default + Debug,
+        M: Module<T>,
+        Vec<T>: GetType,
+{
+    pub fn new(modules: Vec<M>) -> Self {
+        let mut self_modules = vec![];
+        for m in modules {
+            self_modules.push(m);
+        }
+        Self {
+            modules: self_modules,
+            _p: PhantomData,
+        }
+    }
+}
+
+impl<M, T> Module<T> for Sequential<M, T>
+    where
+        M: Module<T>,
+        T: TensoriaOps + Clone + Pod + Default + Debug,
+        Vec<T>: GetType,
+{
+    fn forward(&self, x: &Tensor<T>) -> Tensor<T> {
+        let mut res = self.modules[0].forward(x);
+        for m in &self.modules[1..] {
+            res = m.forward(&res);
+        }
+        res
+    }
+
+    fn to_gpu(&self) -> Result<Self, TensoriaError>
+        where
+            Self: Sized,
+    {
+        todo!()
+    }
+
+    fn parameters(&self) -> Vec<Arc<RwLock<Tensor<T>>>> {
+        let mut params = vec![];
+        for module in &self.modules {
+            for param in &module.parameters() {
+                params.push(param.clone())
+            }
+        }
+        params
+    }
+
+    fn zero_grad(&mut self) {
+        for module in &mut self.modules {
+            module.zero_grad()
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::error::TensoriaError;
-    use crate::nn::{Linear, Module};
+    use crate::nn::{Linear, Module, Sequential};
     use crate::tensor::Tensor;
 
     #[test]
@@ -99,6 +160,16 @@ mod test {
         let res = linear.forward(&x.to_gpu()?);
         assert_eq!(res.shape(), vec![10, 2]);
 
+        Ok(())
+    }
+
+    #[test]
+    fn seq() -> Result<(), TensoriaError> {
+        let x = Tensor::new([1, 2], vec![1.0, 1.0])?;
+        let seq = Sequential::new(vec![
+            Linear::new(2, 2)?,
+            Linear::new(2, 1)?]);
+        _ = seq.forward(&x);
         Ok(())
     }
 }
